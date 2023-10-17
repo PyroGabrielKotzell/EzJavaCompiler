bf712007c6bc6066c6195d912cc9e29fbd2f7c2d
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2023-10-17 17:33:28 +0200

    structure changed

--- EzCompiler/Template/run.bat
+++ EzCompiler/Template/run.bat
@@ -1,3 +1,4 @@
-.\..\jdk-21\bin\javac -d .\out\project -sourcepath .\src\ .\src\Main.java
-.\..\jdk-21\bin\java -Dfile.encoding=UTF-8 -classpath .\out\project Main
+@echo off
+.\..\resources\Inst-jdks\jdk-21\bin\javac -d .\out\project -sourcepath .\src\ .\src\Main.java
+.\..\resources\Inst-jdks\jdk-21\bin\java -Dfile.encoding=UTF-8 -classpath .\out\project Main
 cmd /k
